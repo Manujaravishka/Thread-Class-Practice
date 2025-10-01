1eb3620dd2091129ea31f74783c2958a3c915fb2
Manujaravishka <manujaravishka2004@gmail.com>
2025-10-01 10:44:38 +0530

    lambda Commit learning Thred

---
+++ 04-Lambda-Expression/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,22 @@
+package lk.ijse;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() ->{
+            for(int i=0;i<10;i++){
+                System.out.println("Thread 1 is Running");
+            }
+        });
+
+        Thread t2 = new Thread(() ->{
+            for(int i=0;i<10;i++){
+                System.out.println("Thread 2 is Running");
+            }
+        });
+        t1.start();
+        t2.start();
+
+
+    }
+}
